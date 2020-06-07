8d583351968935e90f90405160bf902fe0887df4
José Luis Quintero C <jquinteroc@acm.org>
2020-06-07 00:57:14 +0000

    Intento 2: mover horizontal o vertical

--- laberinto.py
+++ laberinto.py
@@ -1,19 +1,62 @@
 #Leer laberinto
 def mapa(archivo):
-  return [x.split(" ") for x in open(archivo).readlines()]
+    return [x.split(" ") for x in open(archivo).readlines()]
 
 #Buscar un parametro en el laberinto
 def buscar_en_matriz(matriz, cont, para):
     if matriz == []:
-        return (-1,-1)
-    if para in matriz[0]: 
-        return (cont,matriz[0].index(para))
-    return buscar_en_matriz(matriz[1:],cont+1)
+        return (-1, -1)
+    if para in matriz[0]:
+        return (cont, matriz[0].index(para))
+    return buscar_en_matriz(matriz[1:], cont + 1, para)
 
-def mover(Xy, Xx, Py, Px, matriz):
-  if matriz[Py][Px] == '1':
-    return (Xy, Xx)
-  return (Py, Px)   
+"""
+Intento 2: mover derecha-izquierda o arriba-abajo
+"""
+
+#Funcion para moverse a la derecha
+def moverDer(y, x, matriz, movimientos, aux):
+  print(movimientos)
+  if ((x+aux) >= len(matriz[0]) or (x+aux) < 0):
+    pass
+  if matriz[y][x] == '1' or (matriz[y-1][x-aux] != '1' or matriz[y+1][x-aux] != '1'):
     
-#print(len(mapa('matriz.txt')))
-print(buscar_en_matriz(mapa('matriz.txt'),0))
+  #  print("pared horizontal") 
+  #  if(matriz[y][x-aux] != 'Y'):
+  #    moverVer(y,x-aux,matriz,movimientos, 0)
+  #  print(movimientos)
+  
+    pass
+  elif matriz[y][x+1] != '1':
+    moverDer(y,x+1,matriz,movimientos+[[y,x]], 1)
+  elif matriz[y][x-1] != '1':  
+    moverDer(y,x-1,matriz,movimientos+[[y,x]], -1)
+
+def moverVer(y, x, matriz, movimientos, aux):
+  print(movimientos)
+  print(str(x)+' '+ str(y))
+  if ((y+aux) >= len(matriz) or (y+aux) < 0):
+    pass
+  if matriz[y][x] == '1'or (matriz[y-aux][x-1] != '1' or matriz[y-aux][x+1] != '1'):
+    print("pared Vertical")
+    if(matriz[y][x] != 'Y'):
+      moverDer(y,x,matriz,movimientos, 0)
+    print(movimientos)
+    pass
+  elif matriz[y-1][x] != '1':  
+    moverVer(y-1,x,matriz,movimientos+[[y,x]], -1)
+  elif matriz[y+1][x] != '1':
+    moverVer(y+1,x,matriz,movimientos+[[y,x]], 1)
+
+"""
+11 1 1 1 1 1 1 1 1 1 1
+21 0 1 0 0 0 0 x X 1 1
+31 0 1 1 1 1 1 x 1 x 1
+41 0 0 0 0 0 0 x 0 1 1
+51 0 1 1 1 1 1 1 1 1 1
+61 y 0 0 0 0 0 0 0 Y 1
+71 1 1 1 1 1 1 1 1 1 1
+"""
+
+#print(buscar_en_matriz(mapa('matriz.txt'), 0, "X")[0])
+#moverDer(buscar_en_matriz(mapa('matriz.txt'), 0, "X")[0], buscar_en_matriz(mapa('matriz.txt'), 0, "X")[1], mapa('matriz.txt'), [], 0)
