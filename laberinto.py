327ba51db41c0c4d6356a6591a6284a1a7cebce9
camilofour1 <camilofour1@gmail.com>
2020-06-08 01:07:50 +0000

    Movimiento horizontal y movimiento vertical

--- laberinto.py
+++ laberinto.py
@@ -11,112 +11,56 @@ def buscar_en_matriz(matriz, cont, para):
     return buscar_en_matriz(matriz[1:], cont + 1, para)
 
 """
-Intento 2: mover derecha-izquierda o arriba-abajo
-"""
-"""
-#Funcion para moverse a la derecha
-def moverDer(y, x, matriz, movimientos, aux):
-  print(movimientos)
-  if ((x+aux) >= len(matriz[0]) or (x+aux) < 0):
-    pass
-  if matriz[y][x] == '1' or (matriz[y-1][x-aux] != '1' or matriz[y+1][x-aux] != '1'):
-    
-  #  print("pared horizontal") 
-  #  if(matriz[y][x-aux] != 'Y'):
-  #    moverVer(y,x-aux,matriz,movimientos, 0)
-  #  print(movimientos)
-  
-    pass
-  elif matriz[y][x+1] != '1':
-    moverDer(y,x+1,matriz,movimientos+[[y,x]], 1)
-  elif matriz[y][x-1] != '1':  
-    moverDer(y,x-1,matriz,movimientos+[[y,x]], -1)
-
-def moverVer(y, x, matriz, movimientos, aux):
-  print(movimientos)
-  print(str(x)+' '+ str(y))
-  if ((y+aux) >= len(matriz) or (y+aux) < 0):
-    pass
-  if matriz[y][x] == '1'or (matriz[y-aux][x-1] != '1' or matriz[y-aux][x+1] != '1'):
-    print("pared Vertical")
-    if(matriz[y][x] != 'Y'):
-      moverDer(y,x,matriz,movimientos, 0)
-    print(movimientos)
-    pass
-  elif matriz[y-1][x] != '1':  
-    moverVer(y-1,x,matriz,movimientos+[[y,x]], -1)
-  elif matriz[y+1][x] != '1':
-    moverVer(y+1,x,matriz,movimientos+[[y,x]], 1)
-"""
-
-"""
 Intento 3: Segregar los movimientos y controlarlos por una función master
 """
 # Funcion por movimiento
 def moverDerecha(y,x,matriz,movimientos):
-  print(movimientos)
-  if (x+1) >= len(matriz[0]) or (x+1) < 0:
+  if (x+1) >= len(matriz[0]) or (x+1) < 0 or matriz[y][x+1] == '1' or matriz[y][x] == 'Y':
+    mover(y,x,matriz,movimientos+[[y,x]])
     pass
   else:  
     moverDerecha(y,x+1,matriz,movimientos+[[y,x]])
 
 def moverArriba(y,x,matriz,movimientos):
-  print(movimientos)
-  if (y-1) >= len(matriz) or (y-1) < 0:
+  if (y-1) >= len(matriz) or (y-1) < 0 or matriz[y-1][x] == '1' or matriz[y][x] == 'Y':
+    mover(y,x,matriz,movimientos+[[y,x]])
     pass
   else:
     moverArriba(y-1,x,matriz,movimientos+[[y,x]])
     
-def moverIzquieda(y,x,matriz,movimientos):
-  print(movimientos)
-  if (x-1) >= len(matriz[0]) or (x-1) < 0:
+def moverIzquierda(y,x,matriz,movimientos):
+  if (x-1) >= len(matriz[0]) or (x-1) < 0 or matriz[y][x-1] == '1' or matriz[y][x] == 'Y':
+    mover(y,x,matriz,movimientos+[[y,x]])
     pass
   else:
-    moverIzquieda(y,x-1,matriz,movimientos)
+    moverIzquierda(y,x-1,matriz,movimientos+[[y,x]])
 
 def moverAbajo(y, x, matriz, movimientos):
-  print(movimientos)
-  if (y+1) >= len(matriz) or (y+1) < 0:
+  if (y+1) >= len(matriz) or (y+1) < 0 or matriz[y+1][x] == '1'or matriz[y][x] == 'Y':
+    mover(y,x,matriz,movimientos+[[y,x]])
     pass
   else:
-    moverAbajo(y+1, x, matriz, movimientos)
+    moverAbajo(y+1, x, matriz, movimientos+[[y,x]])
 
 ## Funcion principal de movimiento
 def mover(y,x,matriz,movimientos):
-  if(buscar_en_matriz(matriz,0,'X')[1] == buscar_en_matriz(matriz,0,'Y')[1] and buscar_en_matriz(matriz,0,'X')[0] < buscar_en_matriz(matriz,0,'Y')[0] ):
-  #Abajo
-    pass  
-  elif(buscar_en_matriz(matriz,0,'X')[1] == buscar_en_matriz(matriz,0,'Y')[1] and buscar_en_matriz(matriz,0,'X')[0] > buscar_en_matriz(matriz,0,'Y')[0]):
-  #moverArriba
-    pass
-  elif(buscar_en_matriz(matriz,0,'X')[0] == buscar_en_matriz(matriz,0,'Y')[0] and buscar_en_matriz(matriz,0,'X')[1] > buscar_en_matriz(matriz,0,'Y')[1]):
-  #Izquierda
-    pass
-  elif(buscar_en_matriz(matriz,0,'X')[0] == buscar_en_matriz(matriz,0,'Y')[0] and buscar_en_matriz(matriz,0,'X')[1] < buscar_en_matriz(matriz,0,'Y')[1]):
-  #Derecha
-    pass
-  elif(buscar_en_matriz(matriz,0,'X')[0] > buscar_en_matriz(matriz,0,'Y')[0] and buscar_en_matriz(matriz,0,'X')[1] < buscar_en_matriz(matriz,0,'Y')[1]):
-  #Derecha arriba
-    pass
-  elif(buscar_en_matriz(matriz,0,'X')[0] < buscar_en_matriz(matriz,0,'Y')[0] and buscar_en_matriz(matriz,0,'X')[1] < buscar_en_matriz(matriz,0,'Y')[1]):
-  #Derecha abajo   
-    pass
-  elif(buscar_en_matriz(matriz,0,'X')[0] > buscar_en_matriz(matriz,0,'Y')[0] and buscar_en_matriz(matriz,0,'X')[1] > buscar_en_matriz(matriz,0,'Y')[1]):
-  #Izquierda arriba
-    pass
-  elif(buscar_en_matriz(matriz,0,'X')[0] < buscar_en_matriz(matriz,0,'Y')[0] and buscar_en_matriz(matriz,0,'X')[1] > buscar_en_matriz(matriz,0,'Y')[1]):
-  #Izquierda abajo   
-    pass
+  if (y,x)== buscar_en_matriz(matriz,0,'Y'):
+    print(movimientos)
+  elif x == buscar_en_matriz(matriz,0,'Y')[1] and (y < buscar_en_matriz(matriz,0,'Y')[0] and matriz[y+1][x] != '1'):
+    #Abajo
+    moverAbajo(y, x, matriz, movimientos)
+  
+  elif y == buscar_en_matriz(matriz,0,'Y')[0] and (x < buscar_en_matriz(matriz,0,'Y')[1] and matriz[y][x+1] != '1') :
+    #Derecha
+    moverDerecha(y, x, matriz, movimientos)
 
-"""
-11 1 1 1 1 1 1 1 1 1 1
-21 0 1 0 0 0 0 x X 1 1
-31 0 1 1 1 1 1 x 1 x 1
-41 0 0 0 0 0 0 x 0 1 1
-51 0 1 1 1 1 1 1 1 1 1
-61 y 0 0 0 0 0 0 0 Y 1
-71 1 1 1 1 1 1 1 1 1 1
-"""
+  elif x == buscar_en_matriz(matriz,0,'Y')[1] and (y > buscar_en_matriz(matriz,0,'Y')[0] and matriz[y-1][x] != '1'):
+    #moverArriba
+    moverArriba(y, x, matriz, movimientos)
 
-#print(buscar_en_matriz(mapa('matriz.txt'), 0, "X")[0])
-#moverDer(buscar_en_matriz(mapa('matriz.txt'), 0, "X")[0], buscar_en_matriz(mapa('matriz.txt'), 0, "X")[1], mapa('matriz.txt'), [], 0)
+  elif y == buscar_en_matriz(matriz,0,'Y')[0] and (x > buscar_en_matriz(matriz,0,'Y')[1] and matriz[y][x-1] != '1'):
+    #Izquierda
+    moverIzquierda(y, x, matriz, movimientos)
+
+mover(buscar_en_matriz(mapa('matriz.txt'), 0, "X")[0],buscar_en_matriz(mapa('matriz.txt'), 0, "X")[1], mapa('matriz.txt'), [])
+
